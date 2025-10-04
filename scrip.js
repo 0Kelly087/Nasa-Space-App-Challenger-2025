2f96cd33b573b655856285536773b92647c79d85
Alexandra Gutierrez <162755566+0Kelly087@users.noreply.github.com>
2025-10-04 12:03:18 -0500

    Desarrollo Inicial
    Comenzamos con el desarrollo del proyecto para la Hackaton del Nasa Space App 2025

---
+++ scrip.js
@@ -0,0 +1,5 @@
+// Botón iniciar misión con efecto de transición futurista
+document.getElementById('startMission').addEventListener('click', () => {
+  alert("🚀 Transición hacia la Misión Spacial TEMPO iniciada...");
+  // Aquí después podemos enlazar la transición al siguiente módulo o sección
+});
